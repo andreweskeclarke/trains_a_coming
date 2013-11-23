ef8c098ee74614d76e54dc427281777b6ad20e11
andreweskeclarke <andreweskeclarke@gmail.com>
2013-11-23 17:37:39 -0600

    Using bundle to run unicorn

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
-unicorn -c ./unicorn.rb -E development -D
-/etc/init.d/nginx start
+mkdir tmp
+mkdir tmp/pids
+mkdir tmp/sockets
+mkdir log
+bundle exec unicorn -c ./unicorn.rb -E development -D
+sudo /etc/init.d/nginx start
