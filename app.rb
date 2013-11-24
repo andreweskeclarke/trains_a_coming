7a364a3a35583ce269a26fc92207a424e7049f4d
andreweskeclarke <andreweskeclarke@gmail.com>
2013-11-23 19:26:35 -0600

    Fixing some more time questions

--- app.rb
+++ app.rb
@@ -9,27 +9,32 @@ require 'time'
 CTA_HOST = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx"
 CTA_TIME_FORMAT = "%Y%m%d %T"
 CTA_XML_ARRIVAL_TIME = "//eta/arrT"
+STOP_ID = "30272"
+API_KEY = "219f56e6f90949bd88db9fa28aa57d37"
+LIMIT = 1
+
 
 get '/' do
   cta_response = request_train_arrival_time
-  arrival_time = parse_utc_time_from cta_response
+  arrival_time = parse_time_from cta_response
+  arrival_time_utc = convert_cta_time_to_utc arrival_time
   time_diff = arrival_time - Time.now.utc
   json({:time_to_next_train => time_diff})
 end
 
 def request_train_arrival_time
   uri = URI.parse(CTA_HOST)
-  params = {:key => "219f56e6f90949bd88db9fa28aa57d37",
-            :stpid => "30272",
-            :max => 1}
+  params = {:key => API_KEY,
+            :stpid => STOP_ID,
+            :max => LIMIT}
   uri.query = URI.encode_www_form(params)
   Net::HTTP.get(uri)
 end
 
-def parse_utc_time_from response
+def parse_time_from response
   response_xml = Nokogiri::XML.parse response
   arrival_time_text = response_xml.xpath(CTA_XML_ARRIVAL_TIME).text
-  convert_cta_time_to_utc Time.strptime(arrival_time_text + " +0000", CTA_TIME_FORMAT + " %z")
+  Time.strptime(arrival_time_text + " +0000", CTA_TIME_FORMAT + " %z")
 end
 
 def convert_cta_time_to_utc time
